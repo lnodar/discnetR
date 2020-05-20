73fcc1d9238e23310238bdf6538ac7092c877390
Leah Nodar <leahmnodar@gmail.com>
2020-05-20 11:28:36 -0500

    Initial commit for real this time

---
+++ R/get_excl.R
@@ -0,0 +1,1 @@
+# Get a measure of exclusivity for each speaker node (proportion of subject nodes that only this speaker evaluates)
---
+++ R/get_selfdet.R
@@ -0,0 +1,3 @@
+# Get a measure of self-determination for each speaker node (proportion of evals of speaker that come from that speaker)
+
+
